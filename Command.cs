e956256846a4632b2974678c124fa7268ac578cc
Venkst <58278206+venkatraopcareer@users.noreply.github.com>
2025-04-30 15:09:52 +0530

    Update Command.cs

--- Command.cs
+++ Command.cs
@@ -1,4 +1,6 @@
 The Command pattern is a behavioral design pattern that we can use to turn a request into an object that contains all the information about the request.
+
+    https://code-maze.com/command/
 interface ICommand
 {
     void execute();
@@ -46,86 +48,106 @@ class RemoteController
 
 //////////////////////////////////////////////////////////////////////
 
-public interface ICommand1
-{
-    void ExecuteAction();
-}
-public class Product
-{
-    public string Name { get; set; }
-    public int Price { get; set; }
+  public interface ICommand1
+  {
+      void ExecuteAction();
+      void UndoAction();
+  }
+  public class Product
+  {
+      public string Name { get; set; }
+      public int Price { get; set; }
 
-    public Product(string name, int price)
-    {
-        Name = name;
-        Price = price;
-    }
+      public Product(string name, int price)
+      {
+          Name = name;
+          Price = price;
+      }
 
-    public void IncreasePrice(int amount)
-    {
-        Price += amount;
-        Console.WriteLine($"The price for the {Name} has been increased by {amount}$.");
-    }
+      public void IncreasePrice(int amount)
+      {
+          Price += amount;
+          Console.WriteLine($"The price for the {Name} has been increased by {amount}$.");
+      }
 
-    public void DecreasePrice(int amount)
-    {
-        if (amount < Price)
-        {
-            Price -= amount;
-            Console.WriteLine($"The price for the {Name} has been decreased by {amount}$.");
-        }
-    }
+      public void DecreasePrice(int amount)
+      {
+          if (amount < Price)
+          {
+              Price -= amount;
+              Console.WriteLine($"The price for the {Name} has been decreased by {amount}$.");
+          }
+      }
 
-    public override string ToString() => $"Current price for the {Name} product is {Price}$.";
-}
-public class ProductCommand : ICommand1
-{
-    private readonly Product _product;
-    private readonly PriceAction _priceAction;
-    private readonly int _amount;
+      public override string ToString() => $"Current price for the {Name} product is {Price}$.";
+  }
+  public class ProductCommand : ICommand1
+  {
+      private readonly Product _product;
+      private readonly PriceAction _priceAction;
+      private readonly int _amount;
 
-    public ProductCommand(Product product, PriceAction priceAction, int amount)
-    {
-        _product = product;
-        _priceAction = priceAction;
-        _amount = amount;
-    }
+      public ProductCommand(Product product, PriceAction priceAction, int amount)
+      {
+          _product = product;
+          _priceAction = priceAction;
+          _amount = amount;
+      }
 
-    public void ExecuteAction()
-    {
-        if (_priceAction == PriceAction.Increase)
-        {
-            _product.IncreasePrice(_amount);
-        }
-        else
-        {
-            _product.DecreasePrice(_amount);
-        }
-    }
-}
-public class ModifyPrice
-{
-    private readonly List<ICommand1> _commands;
-    private ICommand1 _command;
+      public void ExecuteAction()
+      {
+          if (_priceAction == PriceAction.Increase)
+          {
+              _product.IncreasePrice(_amount);
+          }
+          else
+          {
+              _product.DecreasePrice(_amount);
+          }
+      }
+      public void UndoAction()
+      {
+          if (_priceAction == PriceAction.Increase)
+          {
+              _product.DecreasePrice(_amount);
+          }
+          else
+          {
+              _product.IncreasePrice(_amount);
+          }
+      }
+  }
+  public class ModifyPrice
+  {
+      private readonly List<ICommand1> _commands;
+      private ICommand1 _command;
 
-    public ModifyPrice()
-    {
-        _commands = new List<ICommand1>();
-    }
+      public ModifyPrice()
+      {
+          _commands = new List<ICommand1>();
+      }
 
-    internal void SetCommand(ICommand1 command) => _command = command;
+      internal void SetCommand(ICommand1 command) => _command = command;
 
-    public void Invoke()
-    {
-        _commands.Add(_command);
-        _command.ExecuteAction();
-    }
-}
-public enum PriceAction
-{
-    Increase,
-    Decrease
-}
+      public void Invoke()
+      {
+          _commands.Add(_command);
+          _command.ExecuteAction();
+      }
+      public void UndoActions()
+      {
+          foreach (var command in Enumerable.Reverse(_commands))
+          {
+              command.UndoAction();
+          }
+      }
+
+  }
+  public enum PriceAction
+  {
+      Increase,
+      Decrease
+  }
 Client-
         var modifyPrice = new ModifyPrice();
     var product = new Product("Phone", 500);
@@ -134,6 +156,8 @@ Client-
     Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Increase, 50));
     Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Decrease, 25));
     Console.WriteLine(product);
+  modifyPrice.UndoActions();
+  Console.WriteLine(product);
 
 private static void Execute(Product product, ModifyPrice modifyPrice, ICommand1 productCommand)
 {
